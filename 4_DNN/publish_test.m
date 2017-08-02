f43cf9ea5ac8380e4de3d0e1bb3e3fed4524ba46
김대운 <dwnusa@gmail.com>
2017-08-02 13:10:33 +0900

    added publish_test.m

---
+++ 4_DNN/publish_test.m
@@ -0,0 +1,16 @@
+%% Matlab은 깔끔하게 짜여진 소스의 HTML Publish를 지원합니다!
+%1.깔끔하게 짜세요
+%2.HTML PUBLISH
+a=3;
+b=2;
+c=cos((b:a).*pi);
+figure,plot(c)
+
+%% 제목처리 주석은 단을 나눠줍니다.
+%1.하하하
+%2.하하하
+%3. mantdu.tistory.com
+figure,plot(sin(1:20));
+
+%%
+publish('d.m');
